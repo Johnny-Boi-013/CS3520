1d11476aa7cb16df328b2a6f6b9172e97ec1a228
Jonathan Jiao <jonathanjiao07@login-el8.khoury.northeastern.edu>
2025-05-09 21:16:20 -0400

    Add symbol_name.cpp and Makefile for Practice Activity 2

---
+++ practiceactivity2/symbol_name.cpp
@@ -0,0 +1,24 @@
+/*
+ * Name: Jonathan Jiao
+ * Date: May 9, 2025
+ * Purpose: Print my name ("Jon Jiao") in ASCII art using symbols.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print ASCII Art of my name (Jon Jiao) using symbols
+    // instead of regular plain text.
+    cout << "Here is my name in symbols:\n\n";
+
+    cout << "     ____.                    ____.__               \n";
+    cout << "    |    | ____   ____       |    |__|____    ____  \n";
+    cout << "    |    |/  _ \\ /    \\      |    |  \\__  \\  /  _ \\ \n";
+    cout << "/\\__|    (  <_> )   |  \\ /\\__|    |  |/ __ \\(  <_> )\n";
+    cout << "\\________|\\____/|___|  / \\________|__(____  /\\____/ \n";
+    cout << "                     \\/                   \\/       \n";
+
+    cout << "\n- Jon Jiao\n";
+    return 0;
+}
